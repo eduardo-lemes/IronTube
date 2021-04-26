2d548e8434753ec95e4b910898bf6b7428a61a44
eduardoLemes <eduardo_lsf@outlook.com>
2021-04-25 21:05:26 -0300

    pages structure

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,8 +1,30 @@
 import React, { Component } from "react";
 
 export class signup extends Component {
+    state = {
+        username:'',
+        passwordHash:'',
+        fullname:'',
+        email: "",
+        imageURL: "",
+  
+    };
   render() {
-    return <div>Signup</div>;
+    return <div>
+    <div> 
+        <form>
+          <label>Username</label>
+          <input type='text' name='username' value={this.state.username}></input>
+          <label>Real Name</label>
+          <input type='text' name='fullname' value={this.state.fullname}></input>
+          <label type='text' name='email' value={this.state.email}>Email</label>
+          <input></input>
+          <label>Image</label>
+          <input></input>
+          <label type='text' name='passwordHash' value={this.state.passwordHash}>Password</label>
+          <input></input>
+        </form>
+      </div></div>;
   }
 }
 
